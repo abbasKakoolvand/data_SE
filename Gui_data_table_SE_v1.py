06a148abb25067be5846cde218ac830e35dbf3b2
Abbas <abbaskakoolvand@gmail.com>
2025-02-24 16:59:17 +0330

    add selector to SE

--- Gui_data_table_SE_v1.py
+++ Gui_data_table_SE_v1.py
@@ -65,14 +65,14 @@ from PyQt6.QtGui import QStandardItemModel, QStandardItem
 import pandas as pd
 
 
-def get_last_10_rows(database_name, table_schema, table_name, connection):
+def get_last_10_rows(database_name, table_schema, table_name, connection, n_latest_record=20):
     try:
         # Create a cursor from the connection
         cursor = connection.cursor()
 
         # Define the SQL query
         query = f"""
-        SELECT TOP 20 *
+        SELECT TOP {n_latest_record} *
         FROM {database_name}.{table_schema}.{table_name}
         """
 
@@ -96,7 +96,7 @@ def get_last_10_rows(database_name, table_schema, table_name, connection):
 
         # Define the fallback SQL query
         query = f"""
-                SELECT TOP 20 *
+                SELECT TOP {n_latest_record} *
                 FROM test_SE.myscema.testTable
                 """
 
@@ -123,24 +123,44 @@ class RowDetailDialog(QDialog):
         self.setWindowTitle("Table sample detail")
         # self.setGeometry(150, 150, 400, 300)
         # Create a connection string
-        database = row_data["DataBaseName"]
+        self.database = row_data["DataBaseName"]
+        self.schema = row_data["TableSchema"]
+        self.table_name = row_data["TableName"]
         # connection_string = f"DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
-        connection_string = connection_string_file.replace("db_my_custom", database)
+        connection_string = connection_string_file.replace("db_my_custom", self.database)
         print(connection_string)
+        layout = QVBoxLayout()
+        selector_layout = QHBoxLayout()
+        spacer = QLabel("")
+
+        self.combo_box = QComboBox()
+        self.combo_box.addItems(["20", "50", "100"])  # Adding options
+        self.combo_box.setMaximumWidth(50)
+        self.combo_box.currentTextChanged.connect(self.on_selection_change)
+
+        label_selector = QLabel("Number of records:")
+        selector_layout.addWidget(label_selector)
+        selector_layout.addWidget(self.combo_box)
+        selector_layout.addWidget(spacer)
+        selector_layout.addWidget(spacer)
+
+        self.table_view = QTableView(self)
+        layout.addLayout(selector_layout)
+        layout.addWidget(self.table_view)
+        self.setLayout(layout)
         # Establish a connection
         try:
-            connection = pyodbc.connect(connection_string, timeout=1)
+            self.connection = pyodbc.connect(connection_string, timeout=1)
         except:
             connection_string = f"DRIVER={{SQL Server}};SERVER=Desktop1312;DATABASE=test_SE;UID=sa;PWD=ario.1377"
-            connection = pyodbc.connect(connection_string, timeout=1)
+            self.connection = pyodbc.connect(connection_string, timeout=1)
 
-        last_10_rows, column_names = get_last_10_rows(database, row_data["TableSchema"], row_data["TableName"],
-                                                      connection)
+        last_10_rows, column_names = get_last_10_rows(self.database, self.schema, self.table_name,
+                                                      self.connection, n_latest_record=20)
         # Create a layout for the dialog
-        layout = QVBoxLayout()
 
         # Create a table view to display the row data
-        self.table_view = QTableView(self)
+
         self.model = QStandardItemModel(0, len(column_names))  # 5 rows, number of columns based on row_data
         self.model.setHorizontalHeaderLabels(column_names)  # Set headers to the keys of the row data
 
@@ -168,11 +188,44 @@ class RowDetailDialog(QDialog):
 
         print(total_width + 100)
         # Add the table view to the layout
-        layout.addWidget(self.table_view)
-        self.setLayout(layout)
 
         # Adjust the dialog size to fit the content
         # self.adjustSize()  # Adjust the size of the dialog to fit its contents
+
+    def on_selection_change(self, value):
+        self.model.removeRows(0, self.model.rowCount())
+        last_10_rows, column_names = get_last_10_rows(self.database, self.schema, self.table_name,
+                                                      self.connection, n_latest_record=value)
+        # Create a layout for the dialog
+
+        # Create a table view to display the row data
+
+        self.model = QStandardItemModel(0, len(column_names))  # 5 rows, number of columns based on row_data
+        self.model.setHorizontalHeaderLabels(column_names)  # Set headers to the keys of the row data
+
+        # Fill the model with row data
+        for row in last_10_rows:
+            if any(item is not None for item in row):  # Adjust this condition as needed
+                items = [QStandardItem(str(item) if item is not None else '') for item in
+                         row]  # Convert None to empty string
+                self.model.appendRow(items)
+
+        self.table_view.setModel(self.model)
+
+        # Resize columns to fit content
+        total_width = 0
+        for col in range(self.model.columnCount()):
+            self.table_view.resizeColumnToContents(col)
+            current_width = self.table_view.columnWidth(col)
+            # Set the column width to the maximum of 200 and the content width
+            self.table_view.setColumnWidth(col, min(200, current_width))
+            current_width = self.table_view.columnWidth(col)
+            total_width = total_width + current_width
+        # self.setGeometry(150, 150, total_width + 100, 300)
+        self.setMinimumWidth(min(total_width + 100, 1000))
+        self.setMaximumWidth(min(total_width + 100, 1000))
+
+        print(total_width + 100)
 
 
 class SearchApp(QMainWindow):
